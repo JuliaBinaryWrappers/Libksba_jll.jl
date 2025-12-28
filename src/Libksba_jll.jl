5e52b9c4a2d8c889cdc9a2c721733aa43458ffd5
jlbuild <juliabuildbot@gmail.com>
2025-12-28 21:51:39 +0000

    Libksba_jll build 1.6.8+0

--- src/Libksba_jll.jl
+++ src/Libksba_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Libksba")
-JLLWrappers.@generate_main_file("Libksba", UUID("52a58d30-3731-5a3f-8361-26ffb4f63669"))
+JLLWrappers.@generate_main_file("Libksba", Base.UUID("52a58d30-3731-5a3f-8361-26ffb4f63669"))
 end  # module Libksba_jll
